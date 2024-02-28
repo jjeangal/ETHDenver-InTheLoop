9d5f2dd0cde66354a4db78da08fbc01cc26c45af
JEAN GAL <jgatbkk@gmail.com>
2024-02-28 15:56:14 -0700

    add route for api call

---
+++ src/app/ipfs/route.ts
@@ -0,0 +1,24 @@
+import pinataSDK from "@pinata/sdk";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const jwt = process.env.PINATA_API_JWT || "";
+
+export async function POST(request: NextApiRequest) {
+    const { id, name, genre, author, contactInfo, artists, nature } = request.body;
+
+    const pinata = new pinataSDK({ pinataJWTKey: jwt });
+
+    const songJson = {
+        id: id,
+        name: name,
+        genre: genre,
+        author: author,
+        contactInfo: contactInfo,
+        artists: artists,
+        nature: nature,
+    };
+
+    const response = await pinata.pinJSONToIPFS(songJson, { pinataMetadata: { name: "song" + id + ".json" } });
+
+    return Response.json(response)
+};
