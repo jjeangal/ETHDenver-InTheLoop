73a6ba4d6688ee5176cb59c517a195f0ebafedc1
JEAN GAL <jgatbkk@gmail.com>
2024-02-28 17:28:08 -0700

    header button

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,21 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
+import { ConnectKitButton } from "connectkit";
+
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <Flex flexDirection="row">
+            <Box>
+                <Text>In the loop</Text>
+            </Box>
+            <ConnectKitButton.Custom>
+                {({ isConnected, isConnecting, show, hide, address, ensName, chain }) => {
+                    return (
+                        <button onClick={show}>
+                            {isConnected ? address : "Custom Connect"}
+                        </button>
+                    );
+                }}
+            </ConnectKitButton.Custom>
+        </Flex>
     )
 }   
