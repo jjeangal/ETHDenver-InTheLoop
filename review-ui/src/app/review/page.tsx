db1389551d68bbe09f483e6fd6b3905ceb2fbb5c
KramPiotr <piotrek@krambit.pl>
2024-03-01 23:55:22 -0700

    RPC calls work up to the voting stage

--- review-ui/src/app/review/page.tsx
+++ review-ui/src/app/review/page.tsx
@@ -2,14 +2,13 @@
 // pages/index.tsx
 import CompileSongsTile from "@/components/CompareSongsTile";
 import { Box, Flex, Grid } from "@chakra-ui/react";
-import { useAccount, useWatchContractEvent } from "wagmi";
-import { reviewContract } from "../assets/reviewContract";
 import {
-  assignRandomSeed,
-  getSubmission,
-  revealResult,
-  selectReviewers,
-} from "../../contract_interaction/calls";
+  useAccount,
+  useWatchContractEvent,
+  useWriteContract,
+  useReadContract,
+} from "wagmi";
+import { reviewContract } from "../assets/reviewContract";
 import { useState } from "react";
 
 const tiles = [
@@ -23,7 +22,58 @@ const Home: React.FC = () => {
   // submissionIds, setSubmissionIds = useState([]);
 
   const account = useAccount();
-  const { accountToReviews, setAccountToReviews } = useState<any>({});
+  const [ accountToReviews, setAccountToReviews ] =
+    useState < Map<string, object[]> >(new Map());
+  const { writeContractAsync } = useWriteContract();
+
+  async function assignRandomSeed(subId: number) {
+    const hash = await writeContractAsync({
+      abi: reviewContract.abi,
+      address: reviewContract.address,
+      functionName: "assignRndSeed",
+      args: [subId],
+    });
+    console.log(`Assign Random Seed --> ${hash}`);
+  }
+
+  async function selectReviewers(subId: number) {
+    const hash = await writeContractAsync({
+      abi: reviewContract.abi,
+      address: reviewContract.address,
+      functionName: "selectReviewers",
+      args: [subId],
+    });
+    console.log(`Select Reviewers --> ${hash}`);
+  }
+
+  async function castVote(subId: number, option: number) {
+    const hash = await writeContractAsync({
+      abi: reviewContract.abi,
+      address: reviewContract.address,
+      functionName: "castVote",
+      args: [subId, option],
+    });
+    console.log(`Cast Vote --> ${hash}`);
+  }
+
+  async function revealResult(subId: number) {
+    const hash = await writeContractAsync({
+      abi: reviewContract.abi,
+      address: reviewContract.address,
+      functionName: "revealResult",
+      args: [subId],
+    });
+    console.log(`Reveal Result --> ${hash}`);
+  }
+
+  function getSubmission(subId: number) {
+    return useReadContract({
+      abi: reviewContract.abi,
+      address: reviewContract.address,
+      functionName: "getSubmission",
+      args: [subId],
+    });
+  }
 
   const { address, abi } = reviewContract;
 
@@ -56,10 +106,22 @@ const Home: React.FC = () => {
     onLogs(logs) {
       const subId = logs[0].args.submissionId;
       const reviewers = logs[0].args.reviewers;
-      console.log(`Reviewers chosen: ${subId}, ${JSON.stringify(reviewers, null, 2)}`);
+      console.log(
+        `Reviewers chosen: ${subId}, ${JSON.stringify(reviewers, null, 2)}`
+      );
       const data = getSubmission(subId);
-
-      selectReviewers(subId);
+      console.log(`Submission data for ${subID} is ${JSON.stringify(data, null, 2)}`)
+      setAccountToReviews((state: Map<string, object[]>) => {
+        for (const reviewer of reviewers) {
+          if (!state.has(reviewer)) {
+            state.set(reviewer, [data]);
+          } else {
+            state.get(reviewer)?.push(data);
+          }
+        }
+        console.log(`Account to reviews ${JSON.stringify(state, null, 2)}`);
+        return state;
+      });
     },
   });
 
@@ -69,6 +131,7 @@ const Home: React.FC = () => {
     eventName: "VotingCompleted",
     onLogs(logs) {
       const subId = logs[0].args.submissionId;
+      console.log(`Voting completed ${subId}`);
       revealResult(subId);
     },
   });
@@ -80,6 +143,7 @@ const Home: React.FC = () => {
     onLogs(logs) {
       const subId = logs[0].args.submissionId;
       const result = logs[0].args.result;
+      console.log(`Result for ${subId} is ${result}`);
       //close the review process
     },
   });
@@ -96,11 +160,11 @@ const Home: React.FC = () => {
     >
       <Box p={4}>
         <Grid templateColumns="repeat(auto-fill, minmax(305px, 1fr))" gap={4}>
-          {tiles.map((tile, index) => (
+          {accountToReviews.get(account.address as string)?.map((review, index) => (
             <CompileSongsTile
               key={index}
-              song1={[50, 60]}
-              song2={[40, 70, 50]}
+              song1={review[1][0]}
+              song2={review[1][1]}
             />
           ))}
         </Grid>
